1ee322cbc86a5d016af69c68a78fdfc094867d3f
Jai Kansra <jaikansr@Jais-MacBook-Pro.local>
2022-07-12 23:28:05 +0530

    added file system for caching and error handling

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,14 @@ app.get('/image-resize', (req, res): void => {
     const fileName = (req.query.fileName as unknown) as string;
     const width = (req.query.width as unknown) as string;
     const height = (req.query.height as unknown) as string;
-    resizeImage(fileName, width, height);
-    res.send('hello')
-    console.log(width, height, fileName);
+    resizeImage(fileName, width, height).then(value => {
+        const path = (value as unknown) as string;
+        try {
+            res.sendFile(path);
+        }
+        catch (error) {
+            res.send(path);
+            console.log(error);
+        }
+    });
 });
--- src/resize.ts
+++ src/resize.ts
@@ -1,8 +1,20 @@
 import sharp from 'sharp';
+import path from 'path';
+import fs from 'fs';
 
-const resizeImage = async (fileName: string, width: string, height: string): Promise<void> => {
+const resizeImage = async (fileName: string, width: string, height: string): Promise<string | undefined> => {
     try {
-        await sharp(`../assets/full/${fileName}.jpeg`).resize(Number(width), Number(height)).toFile(`../assets/thumb/${fileName}-resized.jpeg`);
+        const originalPath: string = path.join(__dirname, `../assets/full/${fileName}.jpeg`);
+        const resizedPath: string = path.join(__dirname, `../assets/thumb/${fileName}-resized.jpeg`);
+        if (!fs.existsSync(originalPath)) {
+            return "No file exists with this name";
+        }
+        if (fs.existsSync(resizedPath)) {
+            console.log("Resized File Already Exists");
+            return resizedPath;
+        }
+        await sharp(originalPath).resize(Number(width), Number(height)).toFile(resizedPath);
+        return resizedPath;
     }
     catch (error) {
         console.log(error);
