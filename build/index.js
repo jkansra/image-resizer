6c1bcf84c97bb3cebc0a42d34030ca98a5269306
Jai Kansra <jaikansr@Jais-MacBook-Pro.local>
2022-07-13 13:35:10 +0530

    fixed some use cases using image-size package, added eslint and prettier configurations

--- build/index.js
+++ build/index.js
@@ -4,8 +4,29 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const resize_1 = __importDefault(require("./resize"));
 const app = (0, express_1.default)();
 const port = 3000;
 app.listen(port, () => {
-    console.log("Listening on Port:", port);
+    console.log('Listening on Port:', port);
+});
+app.get('/image-resize', (req, res) => {
+    const fileName = req.query.fileName;
+    const width = req.query.width;
+    const height = req.query.height;
+    // Check for valid dimensions
+    if (isNaN(Number(width)) || isNaN(Number(height))) {
+        res.send('Please enter valid dimensions');
+        return;
+    }
+    (0, resize_1.default)(fileName, Number(width), Number(height)).then((value) => {
+        const path = value;
+        try {
+            res.sendFile(path);
+        }
+        catch (error) {
+            res.send(path);
+            console.log(error);
+        }
+    });
 });
---
+++ build/resize.js
@@ -0,0 +1,43 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const sharp_1 = __importDefault(require("sharp"));
+const path_1 = __importDefault(require("path"));
+const fs_1 = __importDefault(require("fs"));
+const image_size_1 = __importDefault(require("image-size"));
+const resizeImage = (fileName, width, height) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const originalPath = path_1.default.join(__dirname, `../assets/full/${fileName}.jpeg`);
+        const resizedPath = path_1.default.join(__dirname, `../assets/thumb/${fileName}-resized.jpeg`);
+        if (!fs_1.default.existsSync(originalPath)) {
+            return 'No file exists with this name';
+        }
+        // Update image if different dimensions are passed for same image
+        // Fetching dimensions using image-size package
+        (0, image_size_1.default)(resizedPath, (error, dimensions) => {
+            if (fs_1.default.existsSync(resizedPath) &&
+                (dimensions === null || dimensions === void 0 ? void 0 : dimensions.width) === width &&
+                dimensions.height === height) {
+                console.log('Resized File Already Exists');
+                return resizedPath;
+            }
+        });
+        yield (0, sharp_1.default)(originalPath).resize(width, height).toFile(resizedPath);
+        return resizedPath;
+    }
+    catch (error) {
+        console.log(error);
+    }
+});
+exports.default = resizeImage;
